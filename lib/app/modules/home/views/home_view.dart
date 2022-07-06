2c92d34355520fe0348898012da293a9b22d59cd
Aguspermadi <agsperr@gmail.com>
2022-07-06 18:28:47 +0700

    commit 3

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:taks_management_app/app/routes/app_pages.dart';
+import 'package:taks_management_app/app/utils/widget/sidebar.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -8,16 +11,24 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+     backgroundColor: Colors.blue[100],
+      body: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: 
+          SideBar(),
+          ),
+           Expanded(
+            flex: 15,
+            child: 
+          Container(
+            color: Colors.white,
+          ),
+          )
+        ],
+      )
     );
   }
 }
+
